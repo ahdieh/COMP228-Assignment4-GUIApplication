ab3fa3b49ff1181c08141875f24f65fc609be8fa
ahdieh <aemadi@my.centennialcollege.ca>
2015-11-25 17:39:49 -0600

    Create the fixed UI components

--- src/Program.java
+++ src/Program.java
@@ -2,8 +2,14 @@
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		try {
+			CalculateGPA frame = new CalculateGPA();
+			frame.setVisible(true);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
