5cb8efda64614fe066cee798e9e858796d4e89d6
Ahdieh Emadi <ahdieh.e@gmail.com>
2018-01-29 23:15:45 -0500

    Update Program.java

--- src/Program.java
+++ src/Program.java
@@ -1,4 +1,12 @@
-
+/*
+ * Overview: 
+ * ---------
+ * Develop a GUI application that allows a Centennial College 
+ * student to calculate his/her GPA given the current GPA and total 
+ * number of credit hours earned. The student should be allowed to 
+ * enter as many courses as he/she wants. The program must 
+ * calculate and display the GPA for that student.
+ */
 public class Program {
 
 	public static void main(String[] args) {
